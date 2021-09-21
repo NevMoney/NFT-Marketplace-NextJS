0a3bd39415af9a99dbafff88ee418eebd6131eb8
Nev <67838965+NevMoney@users.noreply.github.com>
2021-09-21 17:57:00 -0400

    adding some config stuff

--- nft-marketplace/hardhat.config.js
+++ nft-marketplace/hardhat.config.js
@@ -1,21 +1,52 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle')
+const fs = require('fs')
+require('dotenv').config()
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://speedy-nodes-nyc.moralis.io/${process.env.PROJECT_ID}/polygon/mumbai`,
+      accounts: [],
+    },
+    polygon: {
+      url: `https://speedy-nodes-nyc.moralis.io/${process.env.PROJECT_ID}/polygon/mainnet`,
+      accounts: [],
+    },
+    rinkeby: {
+      url: `https://speedy-nodes-nyc.moralis.io/${process.env.PROJECT_ID}/eth/rinkeby`,
+      accounts: [],
+    },
+    ethMainnet: {
+      url: `https://speedy-nodes-nyc.moralis.io/${process.env.PROJECT_ID}/eth/mainnet`,
+      accounts: [],
+    },
+    bscMainnet: {
+      url: `https://speedy-nodes-nyc.moralis.io/${process.env.PROJECT_ID}/bsc/testnet`,
+      accounts: [],
+    },
+    bscTestnet: {
+      url: `https://speedy-nodes-nyc.moralis.io/${process.env.PROJECT_ID}/bsc/testnet`,
+      accounts: [],
+    },
+  },
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+}

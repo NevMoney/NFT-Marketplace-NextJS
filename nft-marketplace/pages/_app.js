052edd70a7abec68d888e5d5666d811e2e5eebbb
Nev <67838965+NevMoney@users.noreply.github.com>
2021-09-21 19:48:29 -0400

    imported dependencies for the index.js page

--- nft-marketplace/pages/_app.js
+++ nft-marketplace/pages/_app.js
@@ -2,7 +2,30 @@ import '../styles/globals.css'
 import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      {/* using tailwind for styling: border bottom and 6px padding */}
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        {/* adding links: display flex margin-top 4px */}
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-6 text-pink-500">Home</a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-pink-500">Sell Digital Item</a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-pink-500">My Digital Assets</a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-pink-500">Creator Dashboard</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
--- nft-marketplace/pages/index.js
+++ nft-marketplace/pages/index.js
@@ -1,8 +1,15 @@
-import styles from '../styles/Home.module.css'
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from 'web3modal'
+
+import { nftAddress, marketAddress } from '../config'
+import NFTAbi from '../artifacts/contracts/NFT.sol/NFT.json'
+import MarketAbi from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className=""}>
       <h1>Welcome to NFT Marketplace</h1>
     </div>
   )

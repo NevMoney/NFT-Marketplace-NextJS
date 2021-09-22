5b28df8ba2af2fe67d19bdb751490fb4e649cac3
Nev <67838965+NevMoney@users.noreply.github.com>
2021-09-22 07:38:02 -0400

    deployed contracts locally

--- nft-marketplace/pages/index.js
+++ nft-marketplace/pages/index.js
@@ -3,13 +3,13 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from 'web3modal'
 
-import { nftAddress, marketAddress } from '../config'
+import { nftaddress, nftmarketaddress } from '../config'
+
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
 import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
 
 export default function Home() {
-  // setting state: empty array of NFTs which we can set later and loading state which allows use to show/hide the UI
-  const [nft, setNft] = useState([])
+  const [nfts, setNfts] = useState([])
   const [loadingState, setLoadingState] = useState('not-loaded')
 
   useEffect(() => {
@@ -18,9 +18,9 @@ export default function Home() {
 
   async function loadNFTs() {
     const provider = new ethers.providers.JsonRpcProvider()
-    const tokenContract = new ethers.Contract(nftAddress, NFT.abi, provider)
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
     const marketContract = new ethers.Contract(
-      marketAddress,
+      nftmarketaddress,
       Market.abi,
       provider,
     )
@@ -35,7 +35,7 @@ export default function Home() {
           price,
           tokenId: i.tokenId.toNumber(),
           seller: i.seller,
-          tokenOwner: i.tokenOwner,
+          owner: i.owner,
           image: meta.data.image,
           name: meta.data.name,
           description: meta.data.description,
@@ -43,41 +43,36 @@ export default function Home() {
         return item
       }),
     )
-    setNft(items)
+    setNfts(items)
     setLoadingState('loaded')
   }
-
   async function buyNft(nft) {
     const web3Modal = new Web3Modal()
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
-
     const signer = provider.getSigner()
-    const contract = new ethers.Contract(marketAddress, Market.abi, signer)
+    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
 
     const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')
-
     const transaction = await contract.createMarketSale(
-      nftAddress,
+      nftaddress,
       nft.tokenId,
-      { value: price },
+      {
+        value: price,
+      },
     )
     await transaction.wait()
-
     loadNFTs()
   }
-
-  if (loadingState === 'loaded' && !nft.length)
+  if (loadingState === 'loaded' && !nfts.length)
     return <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>
-
   return (
     <div className="flex justify-center">
       <div className="px-4" style={{ maxWidth: '1600px' }}>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {nft.map((nft, i) => (
+          {nfts.map((nft, i) => (
             <div key={i} className="border shadow rounded-xl overflow-hidden">
               <img src={nft.image} />
-
               <div className="p-4">
                 <p
                   style={{ height: '64px' }}
@@ -85,15 +80,13 @@ export default function Home() {
                 >
                   {nft.name}
                 </p>
+                <div style={{ height: '70px', overflow: 'hidden' }}>
+                  <p className="text-gray-400">{nft.description}</p>
+                </div>
               </div>
-
-              <div style={{ height: '70px', overflow: 'hidden' }}>
-                <p className="text-gray-400">{nft.description}</p>
-              </div>
-
               <div className="p-4 bg-black">
                 <p className="text-2xl mb-4 font-bold text-white">
-                  {nft.price} MATIC
+                  {nft.price} ETH
                 </p>
                 <button
                   className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"

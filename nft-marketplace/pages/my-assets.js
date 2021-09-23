d2d4a3ce5031d7db22bf6b3ba677ea529052d911
Nev <67838965+NevMoney@users.noreply.github.com>
2021-09-23 17:45:53 -0400

    Update my-assets.js

--- nft-marketplace/pages/my-assets.js
+++ nft-marketplace/pages/my-assets.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
 import Web3Modal from 'web3modal'
-import axios 
+import axios from 'axios'
 
 import { nftaddress, nftmarketaddress } from '../config'
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json'

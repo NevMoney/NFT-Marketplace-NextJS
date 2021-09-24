58a92e77e62661ded2fc2fbbe9cb66230ca819a8
Nev <67838965+NevMoney@users.noreply.github.com>
2021-09-23 21:08:47 -0400

    removed unused items

--- nft-marketplace/pages/my-assets.js
+++ nft-marketplace/pages/my-assets.js
@@ -3,8 +3,7 @@ import { ethers } from 'ethers'
 import Web3Modal from 'web3modal'
 import axios from 'axios'
 
-import { nftaddress, nftmarketaddress } from '../config'
-import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import { nftmarketaddress } from '../config'
 import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
 
 export default function myAssets() {
